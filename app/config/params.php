d0280f81e4ac496d6ddfac622358cfbbe9ee4aae
marouakas <marouadevweb@gmail.com>
2025-09-16 20:27:31 +0200

    partials + zone dynamiques

---
+++ app/config/params.php
@@ -0,0 +1,8 @@
+<?php
+// 
+
+// Paramètres de connexion
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'readit_2020');
+define('DB_USER', 'root');
+define('DB_PWD', 'root');
---
+++ core/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+try {
+
+$conn = new PDO('mysql:host=' . DB_HOST . ';
+dbname=' .DB_NAME, DB_USER, DB_PWD);
+} catch (PDOException $e){
+    echo $e->getMessage();
+}
